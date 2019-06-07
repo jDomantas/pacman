8f2d02d4713084c027dfe2fc151d49b5111e2542
Domantas Jadenkus <djadenkus@gmail.com>
2019-06-07 08:13:14 +0300

    derp derp

--- build-web.sh
+++ build-web.sh
@@ -1,11 +1,11 @@
+rm -r static
 mkdir static
 
 cd pacman-web-editor
 elm make src/Main.elm --optimize --output ../static/editor.html
 
-cd pacman-web-login
+cd ../pacman-web-login
 elm make src/Main.elm --optimize --output ../static/login.html
 
 cd ..
-rm -r static/images
 cp -r pacman-web-editor/images static/images
